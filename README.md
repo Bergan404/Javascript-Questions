03d9480debc16af4bd2c9db9c25ee1f71016d323
Bergan Oudshoorn <bergan404@gmail.com>
2022-11-14 08:28:45 -0500

    question one

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Javascript-Questions
+Here are some questions that I think would be easy to understand and answer.
+
+**To Run the questions run node questions.js**
---
+++ questions.js
@@ -0,0 +1,12 @@
+
+
+// Print all even numbers from 0 – 10
+
+for (i = 0; i <= 10; i++) {
+    // let's divide the value by 2
+    // if the remainder is zero then it's an even number
+
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
